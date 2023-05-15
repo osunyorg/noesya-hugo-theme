605bb24369dd3bd286efcf8943b690d8d0840761
Olivia206 <olivia.simonet@mmibordeaux.com>
2023-05-15 18:54:07 +0200

    wip focus trap for menu

--- assets/js/design-system/menu.js
+++ assets/js/design-system/menu.js
@@ -18,9 +18,12 @@ class Menu {
             this.toggleMenu();
         });
         window.addEventListener('keydown', (event) => {
-            if (event.keyCode === 27 || event.key === 'Escape') {
+            if (event.keyCode === 27 && this.menuOpened || event.key === 'Escape' && this.menuOpened) {
                 this.toggleMenu(false);
                 this.button.focus();
+            } else if (event.key === "Tab" && this.menuOpened) {
+                // this.innerFocus(event);
+                // event.preventDefault();
             }
         });
     }
@@ -28,9 +31,33 @@ class Menu {
     toggleMenu(open = !this.menuOpened) {
         this.menuOpened = open;
         
-        this.links.forEach(link => {
+        this.links.forEach((link, index) => {
             link.setAttribute('tabindex', this.menuOpened ? '0' : '-1');
+            if (index === 0) {
+                link.focus();
+            }
         });
+    }
+
+    innerFocus(event) {
+        const focusables = 'a, button, input, textarea, select, details, [tabindex], [contenteditable="true"]';
+        const elements = this.menu.querySelectorAll(focusables);
+
+        const focusableInDialog = Array.from(elements).filter(element => element.tabIndex >= 0);
+        const firstFocusable = focusableInDialog[0];
+        const lastFocusable = focusableInDialog.at(-1);
+
+        if (!this.menuOpened) {
+            return;
+        }
+        console.log("step 1")
+        if (!this.menu.contains(event.target) && event.shiftKey) {
+            lastFocusable.focus();
+        }
+        else if (!this.menu.contains(event.target)) {
+            firstFocusable.focus();
+        }
+        // firstFocusable.focus();
     }
 }
 
